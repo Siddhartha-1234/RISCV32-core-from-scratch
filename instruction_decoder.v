30cc168ced43277aabd1f17519550f6d2f592035
Siddhartha Agarwal <siddhartha231206@gmail.com>
2026-07-28 23:16:09 +0530

    added u type and i type instructions

--- instruction_decoder.v
+++ instruction_decoder.v
@@ -20,6 +20,8 @@ module instruction_decoder(
             7'b0010011, 7'b0000011 : imm_ext = {{20{instruction[31]}}, instruction[31:20]};//immediate genration for I-type instructions represents imm[11:0]
             7'b0100011 : imm_ext = { {20{instruction[31]}}, instruction[31:25], instruction[11:7] };//S-type immediate represents imm[11:0](for store instructions)
             7'b1100011 : imm_ext = { {20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0 };//B-type immediate represents imm[12:1]
+            7'b0110111: imm_ext = { instruction[31:12], 12'b0 };// U-Type (LUI - Load Upper Immediate)
+            7'b1101111: imm_ext = { {12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0 };// J-type immediate represents imm[20:1]
             default : imm_ext = 32'b0;
         endcase
     end
